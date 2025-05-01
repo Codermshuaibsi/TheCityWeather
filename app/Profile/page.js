daaa85c00180963d819621f6b23ba61066d72b39
Mohd Shuaib <Heyshuaib43@gmail.com>
2025-05-01 19:40:54 +0530

    page.js

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -20,7 +20,7 @@ const ProfilePage = () => {
         <User size={48} className="text-gray-500 mb-4" />
         <h2 className="text-xl font-semibold mb-2">You&#39;re not logged in</h2>
         <p className="">Please sign in to view your profile.</p>
-        <button  className="bg-green-500 mt-2 lg:mt-50 mx-30 cursor-pointer absolute text-white px-4 py-2 rounded-md flex items-center justify-center gap-2 hover:bg-green-600 transition"
+        <button  className="bg-green-500 sm:mt-20 lg:mt-50 mx-30 cursor-pointer absolute text-white px-4 py-2 rounded-md flex items-center justify-center gap-2 hover:bg-green-600 transition"
        onClick={()=>{
         router.push('/')
        }} >Sign now</button>
